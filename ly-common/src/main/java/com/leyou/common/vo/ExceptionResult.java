37f0ea24e07581cee8aca88918a21523c2b46e7e
LisCodee <1376417539@qq.com>
2020-03-11 21:57:20 +0800

    统一异常处理和工具

---
+++ ly-common/src/main/java/com/leyou/common/vo/ExceptionResult.java
@@ -0,0 +1,19 @@
+package com.leyou.common.vo;
+
+import com.leyou.common.enums.ExceptionEnum;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public class ExceptionResult {
+    private int status;
+    private String msg;
+    private Long timestamp;
+
+    public ExceptionResult(ExceptionEnum em) {
+        this.status = em.getCode();
+        this.msg = em.getMsg();
+        this.timestamp = System.currentTimeMillis();
+    }
+}
